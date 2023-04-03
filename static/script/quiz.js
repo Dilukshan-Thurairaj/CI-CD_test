20eb670991455150d2f27ed20d032a034b83e631
Dilukshan Thurairaj <thurairajdilu123@gmail.com>
2023-04-03 22:23:05 +0530

    no message

--- static/script/quiz.js
+++ static/script/quiz.js
@@ -328,7 +328,8 @@ function passingArray() {
   console.log(s); // Prints the variables to console window, which are in the JSON format
 
   $.ajax({
-    type: "POST",
+    //POST to GET
+    type: "GET",
     url: "/test",
     contentType: "application/json",
     data: JSON.stringify(s),
